45c3f4c79973c0a71d1d00cbf3e04c799306647b
ahazelton <hazeltonadam@gmail.com>
2018-02-25 19:30:45 -0500

    Finished Autocomplete functions

--- src/AutocompleteCopy.java
+++ src/AutocompleteCopy.java
@@ -4,11 +4,28 @@ public class Autocomplete {
     public Autocomplete(Term[] terms)
 
     // Returns all terms that start with the given prefix, in descending order of weight.
-    public Term[] allMatches(String prefix)
-
+    public Term[] allMatches(String prefix) {
+		int r = prefix.length;
+		int i = BinarySearchDeluxe.firstIndexOf(terms, prefix, Terms.byPrefixOrder(r));
+		int size = numberOfMatches(prefix);
+		Term[] matches = new Term[size];
+		for(; i < size; i++) {
+			matches[i] = terms[i];	
+		}	
+		return matches;
+	}
     // Returns the number of terms that start with the given prefix.
-    public int numberOfMatches(String prefix)
+    public int numberOfMatches(String prefix) {
+		int r = prefix.length;
+		int start, end;
 
+		start = BinarySearchDeluxe.firstIndexOf(terms, prefix, Terms.byPrefixOrder(r));	
+		end = BinarySearchDeluxe.lastIndexOf(terms, prefix, Terms.byPrefixOrder(r));
+		if (start < 0 || end < 0) {
+			return 0;	
+		}
+		return end - start + 1;
+	}
     // unit testing (required)
     public static void main(String[] args)   
 }
