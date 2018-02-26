4d16fa17f2a1736202de75d54e4cfaf5a5d80150
HKirkpatrick <HKirkpatrick@users.noreply.github.com>
2018-02-25 19:11:01 -0500

    binary search fixed

--- src/BinarySearchDeluxecopy.java
+++ src/BinarySearchDeluxecopy.java
@@ -13,6 +13,8 @@ public class BinarySearchDeluxe {
                 lo = mid + 1;
             else if (mid == 0 || comparator.compare(key, a[mid-1]) < 0)
                 return mid;
+            else
+                hi = mid -1;
         }
         return -1;
     }
@@ -20,9 +22,26 @@ public class BinarySearchDeluxe {
     // Returns the index of the last key in a[] that equals the search key, or -1 if no such key.
     public static <Key> int lastIndexOf(Key[] a, Key key, Comparator<Key> comparator)
     {
-        
+        int hi = a.length - 1;
+        int lo = 0;
+        int mid;
+        while (hi >= lo) {
+            mid = lo + (hi - lo)/2;
+            if (comparator.compare(key, a[mid]) < 0)
+                hi = mid - 1;
+            else if (comparator.compare(key,a[mid]) > 0)
+                lo = mid + 1;
+            else if (mid == a.length - 1 || comparator.compare(key, a[mid+1]) < 0)
+                return mid;
+            else
+                lo = mid -1;
+        }
+        return -1;
     }
 
     // unit testing (required)
-    public static void main(String[] args)   
+    public static void main(String[] args)
+    {
+        
+    }
 }
